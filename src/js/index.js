899b3803bcef4b78ede45b5d249a2a75b337fe8a
KJozie88 <joziekhanyi@gmail.com>
2024-12-21 22:05:17 +0200

    basic html and basic JS form

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,15 @@
 function generateRecipe(event) {
     event.preventDefault();
 
-    let recipeElement = document.querySelector("#recipe");
-    recipeElement.innerHTML = "Best breakfast recipe";
+        new Typewriter("#recipe", {
+  strings: "Your recipe goes here",
+  autoStart: true,
+  cursor:"",
+  delay: 50
+})
 }
+
+
 
 let recipeformElement = document.querySelector("#recipe-generator-form");
 recipeformElement.addEventListener("submit", generateRecipe);
