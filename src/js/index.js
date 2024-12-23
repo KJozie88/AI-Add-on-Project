8210803cc20eee4c575b5e84382cc5448a9d68d5
KJozie88 <joziekhanyi@gmail.com>
2024-12-24 01:20:30 +0200

    Modified Html and improved JS and CSS

--- src/js/index.js
+++ src/js/index.js
@@ -5,7 +5,7 @@ console.log("recipe generated")
   strings: response.data.answer,
   autoStart: true,
   cursor:"",
-  delay: 50
+  delay: 25
 });
 }
 
@@ -14,9 +14,13 @@ function generateRecipe(event) {
 
 let instructionsInput = document.querySelector("#user-instructions")
 let apiKey = "b30f34a03ff66542oc8t10fb8777f7a6";
-let context = "You are a food expert with a passion for quick and easy meals. Your mission is to generate recipes that can be made in 30 minutes or less. Give the recipe title only in <strong/> at the top then seperated by <br/> and list ingredients in bullet format,seperated by <br/> and number instructions in the recipe, also seperated by <br/>. Be sure to include section headings of Ingredients and Instructions in <stong/> element, keep all other text normal and exclude the use of all astrix. Use the international metric system. Make sure to follow user instructions. <br/> Sign the recipe with 'SheCodes AI' in a <stong/>element at the end.";
+let context = "You are a food expert with a passion for quick and easy meals. Your mission is to generate recipes that can be made in 30 minutes or less. Give the recipe title only in <strong/> at the top then seperated by <br/> and list ingredients in bullet format,seperated by <br/> and number instructions in the recipe, also each seperated by <br/>. Be sure to include section headings of Ingredients and Instructions in <stong/> element, keep all other text normal and exclude the use of all astrix. Use the international metric system. Make sure to follow user instructions. Then add <br/> element and sign the recipe with 'SheCodes AI' in a <stong/>element at the end.";
 let prompt = `user instructions: Generate a recipe about ${instructionsInput.value}`;
 let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+let recipeElement = document.querySelector("#recipe");
+recipeElement.classList.remove("hidden")
+recipeElement.innerHTML = `<div class="generating"> ⏲️ Generating a ${instructionsInput.value} recipe for you! 🍽️ </div>`;
 
 console.log("Generating recipe");
 console.log(`Prompt: ${prompt}`);
@@ -25,7 +29,6 @@ console.log(`Context: ${context}`);
 axios.get(apiURL).then(displayRecipe);
 
 }
-
 
 
 let recipeformElement = document.querySelector("#recipe-generator-form");
