8142ba0cd55c4d5a0aa15d2b4b5e9bdda934e5c2
KJozie88 <joziekhanyi@gmail.com>
2024-12-24 01:26:25 +0200

    Updates JS context

--- src/js/index.js
+++ src/js/index.js
@@ -13,7 +13,7 @@ function generateRecipe(event) {
 
 let instructionsInput = document.querySelector("#user-instructions")
 let apiKey = "b30f34a03ff66542oc8t10fb8777f7a6";
-let context = "You are a food expert with a passion for quick and easy meals. Your mission is to generate recipes that can be made in 30 minutes or less. Give the recipe title only in <strong/> at the top then seperated by <br/> and list ingredients in bullet format,seperated by <br/> and number instructions in the recipe, also each seperated by <br/>. Be sure to include section headings of Ingredients and Instructions in <stong/> element, keep all other text normal and exclude the use of all astrix. Use the international metric system. Make sure to follow user instructions. Then add <br/> element and sign the recipe with 'SheCodes AI' in a <stong/>element at the end.";
+let context = "You are a food expert with a passion for quick and easy meals. Your mission is to generate recipes that can be made in 30 minutes or less. Give the recipe title only in <strong/> at the top then seperated by <br/> and list ingredients in bullet format,all seperated by <br/> and number instructions in the recipe, also all seperated by <br/>. Be sure to include section headings of Ingredients and Instructions in <stong/> element, keep all other text normal and exclude the use of all astrix. Use the international metric system. Make sure to follow user instructions. Then add <br/> element and sign the recipe with 'SheCodes AI' in a <stong/>element at the end.";
 let prompt = `user instructions: Generate a recipe about ${instructionsInput.value}`;
 let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
